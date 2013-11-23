98c7e19e96056794ad88571ac4ca4bfdb7b057e5
KimY <yungookim@gmail.com>
2013-11-22 20:06:24 -0500

    wordsmith and cleanup

--- presentation/dist/scripts/97c10223.main.js
+++
@@ -1,1 +0,0 @@
-(function(){var a;window.presentation={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";return $("#query").spelling({states:!0})}},$(function(){"use strict";return presentation.init()}),a=function(){}}).call(this),this.JST={done:"true"};
